382569a66a968bc5e68aa47949c1c8eff1817e31
Luis Bertran Alvarez <DownloadADuck@users.noreply.github.com>
2021-12-14 09:22:39 +0000

    Python input file is working. Small typo in README.

--- help_scripts/run_handler.py
+++ help_scripts/run_handler.py
@@ -2,52 +2,35 @@ import shutil
 import os
 import subprocess
 from datetime import date
+import inputs
 
 initialDir = '/home/vagrant/gem5-experiments/help_scripts/m5out'
 runsDir = '/home/vagrant/gem5-experiments/my_runs'
-isa = str(input("ISA: ") or "X86")
-l1iSize = str(input("l1i in kB: ") or 16)
-l1dSize = str(input("l1d in kB: ") or 16)
-l1IterationFactor = int(input("l1 Iteration Factor: ") or 1)
-l1Assoc = int(input("l1 Associativity: ") or 2)
-# Arguments
-baseName = str(input("Runs default name: ") or 'default')
-numberOfRuns = int(input("Number of runs: ") or 1)
+i = 0
 
-# Opens the input file to get the number of runs and the different parameters. 
-def inputGetter():
-    with open("handler_inputs.txt") as file:
-        lines = []
-        lines = file.readlines()
-        lines = [line.rstrip() for line in lines]
-        return lines
-
-for i in range(numberOfRuns):
+for l1is, l1ds, l1ia, l1da in zip(inputs.l1i_size, inputs.l1d_size, inputs.l1i_assoc, inputs.l1d_assoc):
     # Runs the gem5 cmd and wait for it to finish
-    cmd = ['/home/vagrant/gem5-experiments/build/'+isa+'/gem5.opt \
-            /home/vagrant/gem5-experiments/configs/example/se.py \
-            --caches \
-            --l1i_size='+l1iSize+'kB \
-            --l1d_size='+l1dSize+'kB \
-            --l1i_assoc=2 \
-            --l1d_assoc=2 \
-            --sys-clock=2GHz \
-            --cpu-clock=2GHz \
-            --mem-type=DDR4_2400_8x8 \
-            --mem-size=2GB \
-            --cmd=/home/vagrant/gem5-experiments/benchmarks/STREAM-master/stream_c.exe']
+    cmd =[  f'/home/vagrant/gem5-experiments/build/{inputs.isa}/gem5.opt', 
+            '/home/vagrant/gem5-experiments/configs/example/se.py',
+            '--caches',
+            f'--l1i_size={l1is}',
+            f'--l1d_size={l1ds}',
+            f'--l1i_assoc={l1ia}',
+            f'--l1d_assoc={l1da}',
+            '--sys-clock=2GHz',
+            '--cpu-clock=2GHz',
+            '--mem-type=DDR4_2400_8x8',
+            '--mem-size=2GB',
+            '--cmd=/home/vagrant/gem5-experiments/benchmarks/STREAM-master/stream_c.exe']
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+    print(subprocess.list2cmdline(cmd))
     for line in p.stdout:
         print(line)
     p.wait()
     print(p.returncode)
 
-    # Change the size of cache between runs
-    l1dSize = str(int(l1dSize) * l1IterationFactor)
-    l1iSize = str(int(l1iSize) * l1IterationFactor)
-
     # Create a new directory for the current run results
-    newRunDir = os.path.join(runsDir, baseName + "_" + str(i))
+    newRunDir = os.path.join(runsDir, inputs.baseName + "_" + str(i))
     # Created the new directory
     if not os.path.exists(newRunDir):
         os.makedirs(newRunDir)
@@ -59,3 +42,4 @@ for i in range(numberOfRuns):
         if os.path.isfile(full_file_name):
             shutil.copy(full_file_name, newRunDir)
     print("Run number: ", i)
+    i += 1
