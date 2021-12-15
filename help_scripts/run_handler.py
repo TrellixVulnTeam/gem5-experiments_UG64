33008edf2e73270c454573cf700cf35bb1bd1ea5
Luis Bertran Alvarez <DownloadADuck@users.noreply.github.com>
2021-12-15 14:11:21 +0000

    added m5out to the repo to work on output of the script

--- help_scripts/run_handler.py
+++ help_scripts/run_handler.py
@@ -9,26 +9,35 @@ initialDir = '/home/vagrant/gem5-experiments/help_scripts/m5out'
 runsDir = '/home/vagrant/gem5-experiments/my_runs'
 i = 0
 
-for l1is, l1ds, l1ia, l1da in zip(inputs.l1i_size, inputs.l1d_size, inputs.l1i_assoc, inputs.l1d_assoc):
-    # Runs the gem5 cmd and wait for it to finish
-    cmd =[  f'/home/vagrant/gem5-experiments/build/{inputs.isa}/gem5.opt', 
-            '/home/vagrant/gem5-experiments/configs/example/se.py',
-            '--caches',
-            f'--l1i_size={l1is}',
-            f'--l1d_size={l1ds}',
-            f'--l1i_assoc={l1ia}',
-            f'--l1d_assoc={l1da}',
-            '--sys-clock=2GHz',
-            '--cpu-clock=2GHz',
-            '--mem-type=DDR4_2400_8x8',
-            '--mem-size=2GB',
-            '--cmd=/home/vagrant/gem5-experiments/benchmarks/STREAM-master/stream_c.exe']
+# subprocess function
+def subProcess(l1is, l1ds, l1ia, l1da)
+    cmd = cmdBuilder(l1is, l1ds, l1ia, l1da)
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
     print(subprocess.list2cmdline(cmd))
     for line in p.stdout:
         print(line)
     p.wait()
     print(p.returncode)
+
+def cmdBuilder(l1is, l1ds, l1ia, l1da)
+    return cmd =[f'/home/vagrant/gem5-experiments/build/{inputs.isa}/gem5.opt', 
+                '/home/vagrant/gem5-experiments/configs/example/se.py',
+                '--caches',
+                f'--l1i_size={l1is}',
+                f'--l1d_size={l1ds}',
+                f'--l1i_assoc={l1ia}',
+                f'--l1d_assoc={l1da}',
+                '--sys-clock=2GHz',
+                '--cpu-clock=2GHz',
+                '--mem-type=DDR4_2400_8x8',
+                '--mem-size=2GB',
+                '--cmd=/home/vagrant/gem5-experiments/benchmarks/STREAM-master/stream_c.exe']
+
+
+for l1is, l1ds, l1ia, l1da in zip(inputs.l1i_size, inputs.l1d_size, inputs.l1i_assoc, inputs.l1d_assoc):
+
+    # Runs the gem5 cmd and wait for it to finish
+    subProcess(l1is, l1ds, l1ia, l1da)
 
     # Create a new directory for the current run results
     newRunDir = os.path.join(runsDir, inputs.baseName + "_" + str(i))
